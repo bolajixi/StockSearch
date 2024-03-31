815ca95f8b83ddbdfbcf3df23f15f0d0a215699b
Bolaji Olawale <bolaji.olawale007@gmail.com>
2024-03-31 14:14:08 -0700

    Initial Commit

---
+++ StockSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockSearch
+//
+//  Created by Mobolaji Olawale on 3/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockSearch/StockSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockSearchApp.swift
+//  StockSearch
+//
+//  Created by Mobolaji Olawale on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct StockSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
