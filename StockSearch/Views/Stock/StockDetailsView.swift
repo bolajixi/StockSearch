acf5212a68e519a212dc19dbf0bbaaaf6f17dcbf
Bolaji Olawale <bolaji.olawale007@gmail.com>
2024-04-08 16:53:39 -0700

    fix peers link

--- StockSearch/Views/Stock/StockDetailsView.swift
+++ StockSearch/Views/Stock/StockDetailsView.swift
@@ -11,87 +11,97 @@ struct StockDetailsView: View {
     @EnvironmentObject var stockViewModel: StockViewModel
     
     let ticker: String
+    @State private var navigationTitle: String = ""
     
     var body: some View {
         Group {
-            if let stockData = stockViewModel.stockDataResponse {
-                VStack(alignment: .leading) {
-                    Text(stockData.info.name)
-                    
-                    HStack {
-                        Text("$\(String(format: "%.2f", stockData.summary.current))")
-                        Text("$\(String(format: "%.2f", stockData.summary.change))")
-                        Text("(\(String(format: "%.2f", stockData.summary.changePercentage))%)")
-                    }
-                    
-                    // Portfolio Section
+            if stockViewModel.isLoading {
+                VStack{
+                    ProgressView()
+                    Text("Fetching Data...")
+                        .foregroundColor(Color.gray)
+                }
+            } else if let stockData = stockViewModel.stockDataResponse {
+                ScrollView {
                     VStack(alignment: .leading) {
-                        Text("Portfolio")
-                            .font(.headline)
+                        Text(stockData.info.name)
                         
-                        HStack{
-                            Text("You have 0 shares of \(ticker.uppercased()).\nStart trading!")
-                            
-                            Button(action: {
-                                print("bought stock")
-                            }, label: {
-                                Text("Trade")
-                            })
-                        }
-                    }
-                    
-                    // Stats section
-                    VStack(alignment: .leading) {
-                        Text("Stats")
-                        
-                        HStack{
-                            Text("High Price: $\(String(format: "%.2f", stockData.summary.high))")
-                            Text("Open Price: $\(String(format: "%.2f", stockData.summary.open))")
-                        }
-                        HStack{
-                            Text("Low Price: $\(String(format: "%.2f", stockData.summary.low))")
-                            Text("Prev. Close: $\(String(format: "%.2f", stockData.summary.previousClose))")
-                        }
-                    }
-                    
-                    // About section
-                    VStack(alignment: .leading) {
-                        Text("About")
-                        
-                        Text("IPO Start Date: \(stockData.info.ipo)")
-                        Text("Industry: \(stockData.info.finnhubIndustry)")
-                        Text("Webpage: \(stockData.info.weburl)")
                         HStack {
-                            Text("Peers: ")
-                            ScrollView(.horizontal) {
-                                ForEach(stockData.peers, id: \.self) { peer in
-                                    Text(peer)
-                                        .underline()
-                                        .foregroundColor(.blue)
-                                        .onTapGesture {
-                                            if let url = URL(string: peer) {
-                                                UIApplication.shared.open(url)
+                            Text("$\(String(format: "%.2f", stockData.summary.current))")
+                            Text("$\(String(format: "%.2f", stockData.summary.change))")
+                            Text("(\(String(format: "%.2f", stockData.summary.changePercentage))%)")
+                        }
+                        
+                        // Portfolio Section
+                        VStack(alignment: .leading) {
+                            Text("Portfolio")
+                                .font(.headline)
+                            
+                            HStack{
+                                Text("You have 0 shares of \(ticker.uppercased()).\nStart trading!")
+                                
+                                Button(action: {
+                                    print("bought stock")
+                                }, label: {
+                                    Text("Trade")
+                                })
+                            }
+                        }
+                        .padding(.vertical, 15)
+                        .padding(.horizontal, 15)
+                        
+                        // Stats section
+                        VStack(alignment: .leading) {
+                            Text("Stats")
+                            
+                            HStack{
+                                Text("High Price: $\(String(format: "%.2f", stockData.summary.high))")
+                                Text("Open Price: $\(String(format: "%.2f", stockData.summary.open))")
+                            }
+                            HStack{
+                                Text("Low Price: $\(String(format: "%.2f", stockData.summary.low))")
+                                Text("Prev. Close: $\(String(format: "%.2f", stockData.summary.previousClose))")
+                            }
+                        }
+                        
+                        // About section
+                        VStack(alignment: .leading) {
+                            Text("About")
+                            
+                            Text("IPO Start Date: \(stockData.info.ipo)")
+                            Text("Industry: \(stockData.info.finnhubIndustry)")
+                            Text("Webpage: \(stockData.info.weburl)")
+                            HStack {
+                                Text("Peers: ")
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack {
+                                        ForEach(stockData.peers, id: \.self) { peer in
+                                            NavigationLink(destination: StockDetailsView(ticker: peer.lowercased())) {
+                                                Text("\(peer), ")
+                                                    .foregroundColor(.blue)
                                             }
+                                            .onAppear {
+                                                self.navigationTitle = self.ticker.uppercased()
+                                            }
+
                                         }
+                                    }
+                                    .padding()
                                 }
                             }
                         }
+                        
+                        // Insights Section
+                        VStack(alignment: .leading) {
+                            Text("insights")
+                        }
                     }
-                    
-                    // Insights Section
-                    VStack(alignment: .leading) {
-                        Text("insights")
+                    .navigationTitle(navigationTitle)
                     }
+                } else {
+                    Text("Failed to fetch data for \(ticker)")
                 }
-                .navigationTitle(ticker.uppercased())
-            } else if stockViewModel.isLoading {
-                ProgressView()
-                Text("Fetching Data...")
-                    .foregroundColor(Color.gray)
-            } else {
-                Text("Failed to fetch data for \(ticker)")
             }
-        }
         .onAppear {
             stockViewModel.fetchData(forTicker: ticker) { _ in }
         }
