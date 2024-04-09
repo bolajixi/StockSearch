6acfbf1ed53ff21e16518c9f94581718c5b3e314
Bolaji Olawale <bolaji.olawale007@gmail.com>
2024-04-09 16:36:04 -0700

    Add portfolio trading success view

---
+++ StockSearch/Views/Portfolio/PortfolioTradingSuccessView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioTradingSuccessView.swift
+//  StockSearch
+//
+//  Created by Mobolaji Olawale on 4/9/24.
+//
+
+import SwiftUI
+
+struct PortfolioTradingSuccessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PortfolioTradingSuccessView()
+}
